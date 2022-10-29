835e852bcb90cf7305fec606ff9018c1c942a35f
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2022-10-28 22:45:06 -0300

    Função sair

--- sistema-rh/src/view/home/home.jsx
+++ sistema-rh/src/view/home/home.jsx
@@ -1,11 +1,25 @@
 import './home.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
+import { sair } from "../../controller/userController";
 
 export default function Home() {
+
+    const navigate = useNavigate();
+
+    async function enviar(ev) {
+        ev.preventDefault();
+        await sair().then((res) => {
+            if (res) {
+                navigate('/');
+            }
+        });
+    };
+
     return (
         <div className="card-formulario-home">
-            <Form>
+            <Form onSubmit={enviar}>
                 <div className="conteudo-formulario-home">
                     <h3 className="titulo-formulario-home">Home</h3>
 
